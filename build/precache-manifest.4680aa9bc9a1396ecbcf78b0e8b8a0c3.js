fc80cbe7eceed27d61303e3f2a9fdf803b20c47e
Ramireddy Palivela <Ram0206@users.noreply.github.com>
2019-09-07 22:36:18 +0530

    my first app
    dsfsdf

---
+++ build/precache-manifest.4680aa9bc9a1396ecbcf78b0e8b8a0c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f78159ecbf55ef5fec7a944b1594e823",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee00001abbcf8609ccf2",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "1873d012ceb1f6a3b7b7",
+    "url": "/static/js/2.5bc0451a.chunk.js"
+  },
+  {
+    "revision": "ee00001abbcf8609ccf2",
+    "url": "/static/js/main.dca5ad18.chunk.js"
+  },
+  {
+    "revision": "474f79572f729dca25cd",
+    "url": "/static/js/runtime~main.b56cd54a.js"
+  }
+]);
